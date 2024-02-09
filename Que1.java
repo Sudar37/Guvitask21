68e67b9107810a14baa3cd5dfee1c5e8236f03df
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-02-09 12:59:10 +0530

    Update Que1.java

--- Que1.java
+++ Que1.java
@@ -9,37 +9,36 @@ public class Que1 {
 
 	public static void main(String[] args) {
 
-		// Create a new instance of ChromeDriver
+	// Create a new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         
         try {
-            // Navigate to the URL
-            driver.get("https://the-internet.herokuapp.com/iframe");
+        // Navigate to the URL
+        driver.get("https://the-internet.herokuapp.com/iframe");
 
-            // Switch to the iframe using CSS Selector
-            WebElement iframe = driver.findElement(By.cssSelector("iframe#mce_0_ifr"));
-            driver.switchTo().frame(iframe);
+        // Switch to the iframe using CSS Selector
+        WebElement iframe = driver.findElement(By.cssSelector("iframe#mce_0_ifr"));
+        driver.switchTo().frame(iframe);
 
-            // Locate the "p" tag 
-            WebElement pTag = driver.findElement(By.tagName("p"));
-            pTag.clear(); // Clear any existing text
-            pTag.sendKeys("Hello People"); // Enter the desired text
+        // Locate the "p" tag 
+        WebElement pTag = driver.findElement(By.tagName("p"));
+        pTag.clear(); // Clear any existing text
+        pTag.sendKeys("Hello People"); // Enter the desired text
 
-            driver.switchTo().defaultContent();
+        driver.switchTo().defaultContent();
 
-            String enteredText = pTag.getText();
-            if (enteredText.equals("Hello People")) {
-                System.out.println("Text entered successfully: " + enteredText);
-            } else {
-                System.out.println("Text entry failed.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Close the browser
-            //driver.quit();
+        String enteredText = pTag.getText();
+        if (enteredText.equals("Hello People")) {
+        System.out.println("Text entered successfully: " + enteredText);
+        } else {
+        System.out.println("Text entry failed.");
         }
-	}
-
+        } catch (Exception e) {
+        e.printStackTrace();
+        } finally {
+        // Close the browser
+        driver.quit();
+        }
+    }
 }
